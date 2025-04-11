1d196ce6122dde6dadcb4422cb860b92f2daa4ca
Riajul Islam <rjriajul1>
2025-04-11 10:37:21 +0600

    added my first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+// import logo from './assets/food_web_banner_29.jpg'
+
+function App() {
+
+  return (
+    <>
+  
+      <h1 className=''>Vite + React</h1>
+     
+
+    
+    </>
+  )
+}
+
+export default App
